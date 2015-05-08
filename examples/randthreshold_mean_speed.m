587ae9c60d89d09640e12740d1083f07e17d2a86
David Mateo <david.mateo.valderrama@gmail.com>
2015-05-08 15:47:29 +0800

    Two more examples of polarization speed
    randthreshold_mean_speed.m: Use random threshold for each agent,
        uniformly distributed between mint and maxt.
    
    threshold_random_mean_speed.m: Use an undirected random graph
        instead of a small world random graph, i.e. outdegree
        is not constant.

---
+++ examples/randthreshold_mean_speed.m
@@ -0,0 +1,37 @@
+function randthreshold_mean_speed(nag, nleads, kmax , runs, mint, maxt)
+    addpath('../src/graph_generation/')
+    addpath('../src/threshold_consensus/')
+    ks = 01:01:kmax ;
+    fprintf('# Voter consensus\n# Num agents        %i\n# Num leaders       %i\n# Threshold min     %f\n# Threshold max     %f\n# Runs              %i\n',nag, nleads, mint, maxt, runs) ;
+    for k=ks
+        s = zeros(runs,1) ;
+        for r=1:runs
+            % Define small world network
+            g = small_world_graph(nag, k) ;
+            g = regularize_graph(g) ;
+            sp = speed_threshold( nag, nleads, g, mint, maxt) ;
+            s(r) = sp ;
+            end
+        %x = arrayfun( @(r) polarization_voters( nag, nleads, k) , 1:runs) ;
+        fprintf('%i\t%f\t%f\n',k,mean(s),std(s) ) ;
+        end
+    end
+
+function [speed xm] = speed_threshold( nag, nleads, graph, mint, maxt)
+    % Choose random leaders
+    leaders = zeros(1,nag) ;
+    th = mint + (maxt-mint)*rand(1,nag) ;
+    leaders(randperm(nag)(1:nleads)) = ones(1,nleads) ;
+    % Initial state
+    x =zeros(1,nag) ;
+    x(leaders==1) = ones(1,nleads) ;
+
+    niter=5 ;
+    xm = zeros(niter,1) ;
+    for iter=1:niter
+        xold = x ;
+        x = update_leader(graph, x, th, leaders) ;
+        xm(iter) = mean(x(leaders==0)) ;
+        end
+    speed = ols(xm, (1:niter)') ;
+    end
---
+++ examples/threshold_random_mean_speed.m
@@ -0,0 +1,35 @@
+function threshold_random_mean_speed(nag, nleads, dens , runs, threshold)
+    addpath('../src/graph_generation/')
+    addpath('../src/threshold_consensus/')
+    fprintf('# Voter consensus\n# Num agents        %i\n# Num leaders       %i\n# Runs              %i\n',nag, nleads, runs) ;
+    for den=dens
+        s = zeros(runs,1) ;
+        for r=1:runs
+            % Define small world network
+            g = random_undirected_graph(nag, den) ;
+            g = regularize_graph(g) ;
+            sp = speed_threshold( nag, nleads, g, threshold) ;
+            s(r) = sp ;
+            end
+        %x = arrayfun( @(r) polarization_voters( nag, nleads, k) , 1:runs) ;
+        fprintf('%f\t%f\t%f\n',den,mean(s),std(s) ) ;
+        end
+    end
+
+function [speed xm] = speed_threshold( nag, nleads, graph, th)
+    % Choose random leaders
+    leaders = zeros(1,nag) ;
+    leaders(randperm(nag)(1:nleads)) = ones(1,nleads) ;
+    % Initial state
+    x =zeros(1,nag) ;
+    x(leaders==1) = ones(1,nleads) ;
+
+    niter=5 ;
+    xm = zeros(niter,1) ;
+    for iter=1:niter
+        xold = x ;
+        x = update_leader(graph, x, th, leaders) ;
+        xm(iter) = mean(x(leaders==0)) ;
+        end
+    speed = ols(xm, (1:niter)') ;
+    end
