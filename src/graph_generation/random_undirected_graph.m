c8362aaf5907592d2b793c98027920cf3a627e84
David Mateo <david.mateo.valderrama@gmail.com>
2015-05-11 16:26:25 +0800

    random_undirected_graph.m: keep rand distribution
    Symmetrize properly to mantain the distribution of random
    numbers to uniform. A+A'/2 makes the rands non uniform,
    instead copy the bottom part of the matrix into the top part.
    
    Also, in the process remove diagonal because it's "free".

--- src/graph_generation/random_undirected_graph.m
+++ src/graph_generation/random_undirected_graph.m
@@ -1,9 +1,12 @@
 function g = random_undirected_graph(dim, density)
     % Undirected: i connected to j iff j connected to i.
+    % i always disconnected with i.
     % dim       = (int) dimension. the resulting graph
     %             has dim x dim elements.
 	% density   = (float) density of connections, i.e.
 	%             probability of (i,j) to be connected.
 	r = rand(dim) ;
-	g = 0.5*(r + r')<density ;
+    % Dont use Asym = (A+A')/2 because rands stop being uniform.
+    r = tril(r,-1) + tril(r,-1)' ;
+	g = r<density ;
 	end
