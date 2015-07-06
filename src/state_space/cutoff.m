34f8144934465de49b5be5bee69330a0365ab251
David Mateo <david.mateo.valderrama@gmail.com>
2015-07-06 11:52:36 +0800

    cutoff.m: return 0 if no cutoff is found

--- src/state_space/cutoff.m
+++ src/state_space/cutoff.m
@@ -9,6 +9,10 @@ function w = cutoff(sys)
     % doesn't have cutoff frequency.
     [mag pha freqs] = bode(sys) ;
     gain = mag2db(mag) - mag2db(mag(1)) ;
-    w = freqs(gain<-3)(1) ;
+    try
+        w = freqs(gain<-3)(1) ;
+    catch
+        fprintf(stderr,'cutoff.m: No cutoff found, returning zero instead\n') ;
+        w = 0. ;
 end
 
