4d6fae58319e3ac73d2c9360955e22ad0a721cea
David Mateo <david.mateo.valderrama@gmail.com>
2015-10-20 15:06:23 +0800

    Fix cutoff frequency calculation
    Correct i*w etc. Also estimate a candidate cutoff frequency
    by expanding the response function to second order.

--- examples/mean_cutoff.m
+++ examples/mean_cutoff.m
@@ -2,30 +2,14 @@ addpath('../src/graph_generation/')
 addpath('../src/linear_consensus/')
 addpath('../src/state_space/')
 
-totnodes = 50 ;
-k  = 2;
-g = regularmesh_1dperiodic_graph(totnodes, k) ; 
-g = regularize_graph(g) ;
+totnodes = 51 ;
+for k=2:2:50
+    g = regularmesh_1dperiodic_graph(totnodes, k/2) ; 
+    g = regularize_graph(g) ;
 
-
-% Two independent inputs
-leaders=[1 20] ;
-[A B C D] = generate_mimomatrices(g, leaders) ;
-cutoffs = cutoff(A,B,C,D) ;
-mean_cutoff = mean(cutoffs(:)) ;
-fprintf('Mean cutoff frequency:\t%f\n', mean_cutoff)
-
-% Two leaders sharing one input
-is_leader = zeros(1,totnodes) ;
-is_leader(1) = 1 ;
-is_leader(20) = 1 ;
-[A B C D] = generate_sisomatrices(g, is_leader) ;
-nagents = totnodes - sum(is_leader!=0) ;
-cutoffs = zeros(1,nagents) ;
-for i=1:nagents
-    C = zeros(1,nagents) ;
-    C(1,i) = 1 ;
-    cutoffs(i) = cutoff(A,B,C,D) ; 
+    leaders=[1] ;
+    [A B C D] = generate_mimomatrices(g, leaders) ;
+    cutoffs = cutoff(A,B,C,D) ;
+    mean_cutoff = mean(cutoffs(:)) ;
+    printf('%i\t%e\n', k,mean_cutoff)
 end
-mean_cutoff = mean(cutoffs(:)) ;
-fprintf('Mean cutoff frequency:\t%f\n', mean_cutoff)
--- src/state_space/cutoff.m
+++ src/state_space/cutoff.m
@@ -4,7 +4,7 @@ function w = cutoff(A,B,C,D)
     %       dx/dt = A*x + B*u
     %       y     = C*x + D*u
     % The cutoff frequency is defined as
-    % the frequency at which the gain is 1/sqrt(2)
+    % the frequency at which the gain is 1/2
     % times smaller than the gain at w=0.
     %
     % Inputs:
@@ -16,23 +16,24 @@ function w = cutoff(A,B,C,D)
     % Return a matrix of dimension (outs x ins) where
     % w(i,j) is the cutoff frequency of output i to input j.
     
-    one = eye(size(A)) ;
+    onei = i*eye(size(A)) ;
     [nouts nins] = size(D) ;
+    A2 = A*A ;
+    A3 = A2*A ;
     w = zeros(nouts,nins) ;
     for out=1:nouts
         sC = C(out,:) ;
         for in=1:nins
             sB = B(:,in) ;
             sD = D(out,in) ;
-            gain0 = -(sC / A)*sB + sD ; % transfer function at w=0
-            %if (gain0!=1.0)
-            %    printf('multiagent-consensus.cutoff: Gain at w=0 is %f\n', gain0) ;
-            %end
-            target_gain = gain0*sqrt(0.5) ;
+            gain0 = abs( (sC / A)*sB + sD )**2 ; % transfer function at w=0
+            target_gain = gain0/2 ;
             
-            % objective function = Gamma(w) - (1/sqrt(2))Gamma(0)
-            objective = @(w) ( sC /(w*one-A) )*sB + sD - target_gain ;
-            w(out,in) = fzero(objective, 0.0) ;
+            % Cutoff frequency estimated through Taylor expansion to feed fzero:
+            w0 = sqrt(0.5*gain0 / (2.*(sC/A*sB+sD)*(sC/A3*sB)-(sC/A2*sB)**2) ) ;
+            % objective function = Gamma(w) - 0.5*Gamma(0)
+            objective = @(w) ( abs( (sC /(w*onei-A) )*sB + sD)**2 - target_gain );
+            w(out,in) = abs( fzero(objective, w0) ) ;
         end
     end
 end
